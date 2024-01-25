d2d54338470d9e2eec3f1538615091b87bb8aa60
amending1 <joasia.sowiak@gmail.com>
2024-01-25 23:21:04 +0100

    in progress

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,64 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+import Phonebook from './Phonebook.jsx';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+    number: '',
+  };
+
+  handleName = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  };
+  // [event.target.name] - klucz dynamiczny
+  //event.target.value pobiera aktualną wartość wprowadzoną przez użytkownika w polu wejściowym
+
+  handleNumber = event => {
+    this.setState({
+      [event.target.number]: event.target.value,
+    });
+  };
+
+  handleSubmit = event => {
+    //zatrzymuje domyślną akcję przeglądarki, czyli przesłanie formularza i przeładowanie strony
+    event.preventDefault();
+
+    //pobieram nazwę nowego kontaktu i aktualną lista kontaktów
+    const { name, contacts, number } = this.state;
+
+    // Sprawdzanie, czy nazwa nowego kontaktu nie jest pusta - zabezpiecza przed dodaniem pustego pola
+    if (name.trim() === '') return;
+
+    const newContact = {
+      name,
+      id: nanoid(),
+    };
+
+    this.setState({
+      //stan aplikacji jest aktualizowany poprzez dodanie nowego kontaktu do listy kontaktów za pomocą spread operatora
+      contacts: [...contacts, newContact],
+      //wartość nazwy jest resetowana do pustego ciągu, aby wyczyścić pole wejściowe
+      name: '',
+      number: '',
+    });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Phonebook
+          name={this.state.name}
+          number={this.state.number}
+          contacts={this.state.contacts}
+          EnterName={this.handleName}
+          EnterNumber={this.handleNumber}
+          SubmitNewContact={this.handleSubmit}
+        />
+      </div>
+    );
+  }
+}
